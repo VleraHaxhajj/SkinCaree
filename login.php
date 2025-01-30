f8f6c6b95cb451d56de94e939e3b618a67a238af
VleraHaxhajj <spahiuera8@gmail.com>
2025-01-30 10:08:47 +0100

    login ne php

--- login.php
+++ login.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LANEIGE Skincare & Make-up</title>
     <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 </head>
 <body>
@@ -13,28 +12,16 @@
         <a href="homepage.html"><i class='bx bx-arrow-back'></i></a>
     </header>
     <div class="wrapper">
+
         <?php
-<<<<<<< HEAD
-        if(isset($_POST["login"])){
-            $name = $_POST ["name"];
-            $password = $_POST ["password"];
-            require_once "database.php";
-            $sql = "SELECT * FROM users WHERE username = ?";
-            $stmt = mysqli_stmt_init($conn);
-            if(mysqli_stmt_prepare($stmt,$sql)){
-                mysqli_stmt_bind_param($stmt , "s" , $name);
-                mysqli_stmt_execute($stmt);
-                $result + mysqli_stmt_get_result($stmt);
-                $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
-=======
           
           include_once 'Database.php';
           include_once 'User.php';
      
          if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        /* $db = new Database();
+        /$db = new Database();
          $connection = $db->getConnection();
-         $user = new User($connection);*/
+         $user = new User($connection);
      
          
          $name = $_POST['name'];
@@ -50,33 +37,13 @@
      }
    
         ?>
->>>>>>> 78d0c52 (update)
 
-                if($user){
-                    if(password_verify($password,$user['password'])){
-                        header("Location:index.php");
-                        die();
-
-                    }else{
-                        echo"<div class='alert alert-danger'>Password does not match</div>";
-                    }
-
-                    }else{
-                        echo"<div class='alert alert-danger'>Username doesn not match</div>";
-                    }
-
-                }else{
-                    echo"<div class='alert alert-danger>This username does not exist</div>";
-                }
-        }
-         ?>
-         
         <h1>Login</h1>
         <div class="input-box">
-            <input type="text" id="name" placeholder="Enter your email" required>
+            <input type="text" placeholder="Enter your email" required>
         </div>
         <div class="input-box">
-            <input type="password" id="password" placeholder="Enter your password" required>
+            <input type="password" placeholder="Enter your password" required>
         </div>
         <div class="remember-forgot">
             <label><input type="checkbox">Remember me</label>
@@ -84,11 +51,13 @@
         </div>
         <button type="submit" class="buton">Login</button>
         <div class="register-link">
-            <p>Don't have an account?
+            <>Don't have an account?
                 <a href="register.html">Register</a>
-            </p>
+    </p>
         </div>
     </div>
     <script src="login.js"></script>
 </body>
 </html>
+
+
