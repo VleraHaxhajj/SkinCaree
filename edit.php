95f088015631ffa2b7ffe492ee7a43c45f3053bb
VleraHaxhajj <spahiuera8@gmail.com>
2025-02-01 17:51:36 +0100

    GJYSEM FINALE

--- edit.php
+++ edit.php
@@ -1,21 +1,32 @@
 <?php
-$userId =$_GET['id'];
+require 'database.php'; 
 
-include_once 'userR.php';
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
+    $id = intval($_GET['id']);
 
-$userR = new userR();
+    $user_sql = "SELECT * FROM users WHERE id = $id";
+    $user_result = $conn->query($user_sql);
 
-$user = $userR->getUserById($userId);
-
-if(isset($_POST['editBtn'])){
-    $id= $user ['Id'];
+    if ($user_result && $user_result->num_rows === 1) {
+        $user = $user_result->fetch_assoc();
+    } else {
+        die("User not found.");
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = intval($_POST['id']);
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
 
-    $userR ->updateUser($id,$name,$email,$password);
-
-    header('location:dashboard.php');
+    
+    $update_sql = "UPDATE users SET name = '$name', email = '$email' WHERE id = $id";
+    if ($conn->query($update_sql)) {
+        header('Location: admindashboard.php');
+        exit();
+    } else {
+        die("Error updating user: " . $conn->error);
+    }
+} else {
+    die("Invalid request.");
 }
 ?>
 
@@ -25,20 +36,18 @@ if(isset($_POST['editBtn'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit User</title>
+    <link rel="stylesheet" href="admindashboard.css">
 </head>
 <body>
-<h3>Edit User</h3>
-<form action="" method="post">
+    <form method="POST" action="edit.php">
+        <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($user['name']); ?>" required>
 
-<input type="text" name="id" value="<?=$user['Id']?> "readonly>  <br> <br>
-        
-        <input type="text" name="name" value="<?=$user['Name']?>"> <br> <br>
-    
-        <input type="text" name="email" value="<?=$user['Email']?>"> <br> <br>
-        
-        <input type="text" name="password" value="<?=$user['Password']?>"> <br> <br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
 
-        <input type="submit" name="editBtn" value="Save Changes"> <br> <br>
+        <button type="submit">Update User</button>
     </form>
 </body>
 </html>
